d601987a0c5449c19d4ca67609c34cc97d67f902
i556san@gmail.com <bvahamut@Bvahamut-2.local>
2017-08-04 14:45:07 +0900

    add i18n

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,10 @@ module Blank
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = [:en, :zh]
+    config.i18n.default_locale = :zh
+    config.encoding = 'utf-8'
   end
 end
